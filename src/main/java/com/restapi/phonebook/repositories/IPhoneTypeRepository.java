a8da3afdbc5bb6a333c075b79633c08ed8050f79
mariokolev <mario.kolevv@gmail.com>
2021-02-08 12:10:55 +0200

    added query method for querying with parameter phonetype

--- src/main/java/com/restapi/phonebook/repositories/IPhoneTypeRepository.java
+++ src/main/java/com/restapi/phonebook/repositories/IPhoneTypeRepository.java
@@ -2,6 +2,13 @@ package com.restapi.phonebook.repositories;
 
 import com.restapi.phonebook.entities.PhoneType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
+@Repository
 public interface IPhoneTypeRepository extends JpaRepository<PhoneType, Long> {
+
+    @Query("SELECT p FROM PhoneType WHERE p.phoneType = ?1")
+    Optional<PhoneType> findPhoneTypeByType(String phoneType);
 }
