68afb0ab6ecfd5674721187ea1697febf56286cb
mariokolev <mario.kolevv@gmail.com>
2021-02-08 10:52:43 +0200

    added getPersons() method

--- src/main/java/com/restapi/phonebook/controllers/PersonController.java
+++ src/main/java/com/restapi/phonebook/controllers/PersonController.java
@@ -1,9 +1,13 @@
 package com.restapi.phonebook.controllers;
 
+import com.restapi.phonebook.entities.Person;
 import com.restapi.phonebook.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/persons")
@@ -16,5 +20,8 @@ public class PersonController {
         this.personService = personService;
     }
 
-    
+    @GetMapping
+    public List<Person> getPersons(){
+        return personService.getPersons();
+    }
 }
