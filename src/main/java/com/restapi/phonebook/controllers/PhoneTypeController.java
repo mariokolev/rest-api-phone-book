e4c7d4232b3ab9250a8b223cdb84e76deda3e51d
mariokolev <mario.kolevv@gmail.com>
2021-02-08 11:06:53 +0200

    created POST method for creating phone type

--- src/main/java/com/restapi/phonebook/controllers/PhoneTypeController.java
+++ src/main/java/com/restapi/phonebook/controllers/PhoneTypeController.java
@@ -3,10 +3,7 @@ package com.restapi.phonebook.controllers;
 import com.restapi.phonebook.entities.PhoneType;
 import com.restapi.phonebook.services.PhoneTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +26,9 @@ public class PhoneTypeController {
     @GetMapping(path = "{phoneTypeId}")
     public PhoneType getPhoneTypeById(@PathVariable("phoneTypeId") Long phoneTypeId){
         return phoneTypeService.getPhoneTypeById(phoneTypeId);
+    }
+
+    public void addPhoneType(@RequestBody PhoneType phoneType){
+        phoneTypeService.addPhoneType(phoneType);
     }
 }
